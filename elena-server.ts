0afdc86d074b27ceba8dafdbfea6e5ea55e73830
Dave Beyer <dave.beyer@gmail.com>
2023-04-18 01:32:43 -0700

    Add hello world server

--- elena-server.ts
+++ elena-server.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import xmlFormat from 'xml-formatter';
 
+import express from 'express';
+
+//////////////////////////////////////////////////////
+//
+// Update Dynamic DNS IP address for davesmaze.com
+//
+//////////////////////////////////////////////////////
+
 const domain   = 'davesmaze.com';
 const ddns_key = 'cc708c2396b049e2b91d77361e351156';
 const ddns_url = 'https://dynamicdns.park-your-domain.com/update';
 
-//
-// Update Dynamic DNS IP address for davesmaze.com
-//
 try {
     const resp = await axios.get(ddns_url, { params: {
         host: '@',
@@ -31,3 +36,19 @@ try {
     console.log("ERROR Updating DDNS *: " + JSON.stringify(error));
 }
 
+//////////////////////////////////////////////////////
+//
+// Start Express-node server
+//
+//////////////////////////////////////////////////////
+
+const app = express()
+const port = 3000
+
+app.get('/', (req:any, res:any) => {
+  res.send("Hi from Dave's Maze!")
+});
+
+app.listen(port, () => {
+  console.log(`Elena server listing on port ${port}`)
+})
