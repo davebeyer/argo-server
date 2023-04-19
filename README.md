af82ea5b1aeaac0511129fb31f93f2346140a2ac
Dave Beyer <dave.beyer@gmail.com>
2023-04-18 23:31:59 -0700

    Initial bootstrap site

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Elena Server
 
-Initial import
+
+
+
+## node.js using port 80
+
+How to allow node.js server to bind to port 80.
+See [here](https://stackoverflow.com/questions/6109089/how-do-i-run-node-js-on-port-80).
+
+```
+sudo apt-get install libcap2-bin
+sudo setcap cap_net_bind_service=+ep /usr/bin/node
+```
 
--- elena-server.ts
+++ elena-server.ts
@@ -1,7 +1,12 @@
-import axios from 'axios';
-import xmlFormat from 'xml-formatter';
+// declare module 'express-subdomain';  
 
-import express from 'express';
+const axios = require('axios');
+const xmlFormat = require('xml-formatter');
+
+// @ts-ignore   (required due to lack of @types/express-subdomain)
+const subdomain = require('express-subdomain');
+
+const express = require('express');
 
 //////////////////////////////////////////////////////
 //
@@ -13,28 +18,33 @@ const domain   = 'davesmaze.com';
 const ddns_key = 'cc708c2396b049e2b91d77361e351156';
 const ddns_url = 'https://dynamicdns.park-your-domain.com/update';
 
-try {
-    const resp = await axios.get(ddns_url, { params: {
-        host: '@',
-        domain: domain,
-        password: ddns_key
-    }});
-    
-    console.log("Updating DDNS @: " + resp.status + "\n" + xmlFormat(resp.data));
-} catch (error) {
-    console.log("ERROR Updating DDNS @: " + JSON.stringify(error));
-}
+const updateDDNS = async () => {
+    try {
+        const resp = await axios.get(ddns_url, { params: {
+            host: '@',
+            domain: domain,
+            password: ddns_key
+        }});
+        
+        console.log("Updating DDNS @: " + resp.status + "\n" + xmlFormat(resp.data));
+    } catch (error) {
+        console.log("ERROR Updating DDNS @: " + JSON.stringify(error));
+    }
 
-try {
-    const resp = await axios.get(ddns_url, { params: {
-        host: '*',
-        domain: domain,
-        password: ddns_key
-    }});
-    console.log("Updating DDNS *: " + resp.status +  "\n" + xmlFormat(resp.data));
-} catch (error) {
-    console.log("ERROR Updating DDNS *: " + JSON.stringify(error));
-}
+    try {
+        const resp = await axios.get(ddns_url, { params: {
+            host: '*',
+            domain: domain,
+            password: ddns_key
+        }});
+        console.log("Updating DDNS *: " + resp.status +  "\n" + xmlFormat(resp.data));
+    } catch (error) {
+        console.log("ERROR Updating DDNS *: " + JSON.stringify(error));
+    }
+};
+
+updateDDNS(); // no need to wait
+
 
 //////////////////////////////////////////////////////
 //
@@ -43,12 +53,32 @@ try {
 //////////////////////////////////////////////////////
 
 const app = express()
-const port = 3000
+const port = 80
+
+//
+// Subdomain redirects/proxies
+//
+
+app.use(subdomain('argonauts', (req:any, res:any, next:any) => {
+  console.log(`Argonauts subdomain!`)
+  next();
+}));
+
+//
+// Routes for this site
+//
+
+// Static node_modules routes
+app.use('/nm', express.static('node_modules'))
+
+// Static node_modules routes
+app.use('/assets', express.static('assets'))
+
 
 app.get('/', (req:any, res:any) => {
-  res.send("Hi from Dave's Maze!")
+  res.sendFile('./templates/index.html', { root: __dirname });
 });
 
 app.listen(port, () => {
   console.log(`Elena server listing on port ${port}`)
-})
+});
