e79e5b1552e887ee6f89fa5d539e4999b23e1a39
Dave Beyer <dave.beyer@gmail.com>
2023-04-19 22:16:57 -0700

    Version 0.1 of Argonaut server (without /post proxy)

--- argo-server.ts
+++ argo-server.ts
@@ -1,52 +1,7 @@
-// declare module 'express-subdomain';  
-
 const axios = require('axios');
 const xmlFormat = require('xml-formatter');
 
-// @ts-ignore   (required due to lack of @types/express-subdomain)
-const subdomain = require('express-subdomain');
-
 const express = require('express');
-
-//////////////////////////////////////////////////////
-//
-// Update Dynamic DNS IP address for davesmaze.com
-//
-//////////////////////////////////////////////////////
-
-const domain   = 'davesmaze.com';
-const ddns_key = 'cc708c2396b049e2b91d77361e351156';
-const ddns_url = 'https://dynamicdns.park-your-domain.com/update';
-
-const updateDDNS = async () => {
-    try {
-        const resp = await axios.get(ddns_url, { params: {
-            host: '@',
-            domain: domain,
-            password: ddns_key
-        }});
-        
-        console.log("Updating DDNS @: " + resp.status + "\n" + xmlFormat(resp.data));
-    } catch (error) {
-        console.log("ERROR Updating DDNS @: " + JSON.stringify(error));
-    }
-
-    try {
-        const resp = await axios.get(ddns_url, { params: {
-            host: '*',
-            domain: domain,
-            password: ddns_key
-        }});
-        console.log("Updating DDNS *: " + resp.status +  "\n" + xmlFormat(resp.data));
-    } catch (error) {
-        console.log("ERROR Updating DDNS *: " + JSON.stringify(error));
-    }
-};
-
-if (false) {
-   updateDDNS(); // no need to wait
-}
-
 
 //////////////////////////////////////////////////////
 //
@@ -56,15 +11,6 @@ if (false) {
 
 const app = express()
 const port = 80
-
-//
-// Subdomain redirects/proxies
-//
-
-app.use(subdomain('argonauts', (req:any, res:any, next:any) => {
-  console.log(`Argonauts subdomain!`)
-  next();
-}));
 
 //
 // Routes for this site
