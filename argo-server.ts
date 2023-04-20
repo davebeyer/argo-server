b1ad201dd656488a9c01e3bc66c25810279e4c62
Dave Beyer <dave.beyer@gmail.com>
2023-04-20 03:24:42 -0700

    use redirect and fixed ports for now

--- argo-server.ts
+++ argo-server.ts
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const xmlFormat = require('xml-formatter');
+const morgan = require('morgan');
 
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 //////////////////////////////////////////////////////
 //
@@ -11,6 +13,9 @@ const express = require('express');
 
 const app = express()
 const port = 80
+
+// logging
+app.use(morgan('combined'));
 
 //
 // Routes for this site
@@ -22,11 +27,27 @@ app.use('/nm', express.static('node_modules'))
 // Static node_modules routes
 app.use('/assets', express.static('assets'))
 
+app.use('/post', (req:any, res:any) => {
+   // Until proxy server is working, do a redirect here!
+   let host = req.get('host').split(':')[0];
+   let newUrl = req.protocol + '://' + host + ':4567';
+   console.log("Redirecting to " + newUrl);
+   res.redirect(newUrl);
+});
+
+app.use('/post', createProxyMiddleware({
+  target: 'http://192.168.1.159:4567',
+  changeOrigin: true,
+  ws: true,
+  pathRewrite: { '^/post': '' }
+}));
+
 
 app.get('/', (req:any, res:any) => {
   res.sendFile('./templates/index.html', { root: __dirname });
 });
 
+    
 app.listen(port, () => {
   console.log(`Argo server listening on port ${port}`)
 });
---
+++ assets/js/index.ts
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    // $('.gotoPost').on( "click", function(event:any) {
+    //     event.preventDefault();
+    //
+    //    let host = window.location.host;
+    //    let protocol = window.location.protocol;
+    //
+    //    location.href = `${protocol}//${host}:4567`;
+    // });
+});
