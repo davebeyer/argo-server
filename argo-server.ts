1d2de5d7e35a77e6a772a9623db8c02e71fe6409
Dave Beyer <dave.beyer@gmail.com>
2023-04-20 22:11:11 -0700

    additional tweaks to a solid version

--- argo-server.ts
+++ argo-server.ts
@@ -27,19 +27,14 @@ app.use('/nm', express.static('node_modules'))
 // Static node_modules routes
 app.use('/assets', express.static('assets'))
 
-app.use('/post', (req:any, res:any) => {
-   // Until proxy server is working, do a redirect here!
-   let host = req.get('host').split(':')[0];
-   let newUrl = req.protocol + '://' + host + ':4567';
-   console.log("Redirecting to " + newUrl);
-   res.redirect(newUrl);
-});
-
 app.use('/post', createProxyMiddleware({
-  target: 'http://192.168.1.159:4567',
+  target: 'http://127.0.0.1:4567',
   changeOrigin: true,
   ws: true,
-  pathRewrite: { '^/post': '' }
+  
+  // comment out path rewrite, nodebb expects the /post due
+  // to canonical url setting apparently
+  // pathRewrite: { '^/post': '' }
 }));
 
 
